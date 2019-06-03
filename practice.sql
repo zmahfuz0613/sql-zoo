e536c2544c30d887cefec922af0ae474e0f92625
Rachel Moskowitz <rachelbmoskowitz@gmail.com>
2019-06-03 15:37:34 -0400

    Update practice.sql

--- practice.sql
+++ practice.sql
@@ -1,1 +1,127 @@
-SELECT * FROM cities;
+-- SECTION 0 - SELECT BASICS
+
+--Modify it to show the population of Germany
+1. 
+ 
+ -- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+2. 
+
+-- Modify it to show the country and the area for countries with an area between 200,000 and 250,000.
+3. 
+
+
+-- SECTION 1 - SELECT NAME
+
+--Find the country that start with Y
+1. 
+
+-- Find the countries that end with y
+2. 
+
+-- Find the countries that contain the letter x
+3. 
+
+-- Find the countries that end with land
+4. 
+
+-- Find the countries that start with C and end with ia
+5. 
+
+-- Find the country that has oo in the name
+6. 
+
+-- Find the countries that have three or more a in the name
+7. 
+
+-- Find the countries that have "t" as the second character.
+8. 
+
+-- Find the countries that have two "o" characters separated by two others.
+9. 
+
+-- Find the countries that have exactly four characters.
+10. 
+
+
+-- SECTION 2 - SELECT from World
+
+ -- Observe the result of running this SQL command to show the name, continent and population of all countries.
+1. 
+
+-- Show the name for the countries that have a population of at least 200 million. 
+-- 200 million is 200000000, there are eight zeros.
+2. 
+
+-- Give the name and the per capita GDP for those countries with a population of at least 200 million.
+3. 
+
+-- Show the name and population in millions for the countries of the continent 'South America'. 
+4. 
+
+-- Show the name and population for France, Germany, Italy
+5. 
+
+-- Show the countries which have a name that includes the word 'United'
+6. 
+
+-- Show the countries that are big by area or big by population. Show name, population and area.
+7. 
+
+-- Show the countries that are big by area or big by population but not both. Show name, population and area.
+8. 
+
+-- For South America show population in millions and GDP in billions both to 2 decimal places.
+9. 
+
+-- Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+10. 
+
+-- Show the name and capital where the name and the capital have the same number of characters.
+11. 
+
+-- Show the name and the capital where the first letters of each match. 
+-- Don't include countries where the name and the capital are the same word.
+12. 
+
+-- Find the country that has all the vowels and no spaces in its name.
+13. 
+
+
+
+--SECTION 3 - SELECT FROM nobel
+
+-- Change the query shown so that it displays Nobel prizes for 1950.
+1. 
+
+-- Show who won the 1962 prize for Literature.
+2.
+
+-- Show the year and subject that won 'Albert Einstein' his prize.
+3.
+
+-- Give the name of the 'Peace' winners since the year 2000, including 2000.
+4. 
+
+-- Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+5. 
+
+/*Show all details of the presidential winners:
+Theodore Roosevelt
+Woodrow Wilson
+Jimmy Carter
+Barack Obama*/
+6. 
+
+-- Show the winners with first name John
+7. 
+
+-- Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
+8. 
+
+-- Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
+9. 
+
+-- Show year, subject, and name of people who won a 'Medicine' prize in an early year 
+-- (before 1910, not including 1910) together with winners of a 'Literature' prize in a later year 
+-- (after 2004, including 2004)
+10. 
